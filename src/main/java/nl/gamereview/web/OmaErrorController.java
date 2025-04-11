091a34584efcd4a92a6ddf8881e1f9af26a623fb
bhi116 <bhi116@myy.haaga-helia.fi>
2025-04-11 14:37:48 +0300

    Errorpage

---
+++ src/main/java/nl/gamereview/web/OmaErrorController.java
@@ -0,0 +1,21 @@
+package nl.gamereview.web;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@Controller
+public class OmaErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request, Model model) {
+        Object status = request.getAttribute("jakarta.servlet.error.status_code");
+
+        model.addAttribute("status", status);
+
+        return "error";
+    }
+}
